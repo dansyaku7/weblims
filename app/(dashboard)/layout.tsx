be6dada4a2b81862f42f577e05ce7be09aacd8a7
dansyaku7 <syakuronabdan49@gmail.com>
2025-07-28 20:41:25 +0700

    Reapply "remove import global.css di layout (dashboard)"
    This reverts commit 58981417f58649a4f2b97628ceb44cb37d1b178b.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import { LoadingProvider } from "@/components/context/LoadingContext";
