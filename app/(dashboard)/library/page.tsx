ba6c14212c8d5f90580061bc8fdd8702e86e7ab0
dansyaku7 <syakuronabdan49@gmail.com>
2025-07-28 17:11:52 +0700

    test with console and change ANALIS to analis

--- app/(dashboard)/library/page.tsx
+++ app/(dashboard)/library/page.tsx
@@ -11,11 +11,8 @@ export default async function DataLibraryPage() {
   // --- PERUBAHAN DIMULAI DI SINI ---
   // 1. Ambil data sesi user yang sedang login di server
   const session = await getServerSession(authOptions);
-
-  // 2. Tentukan apakah kolom customer boleh ditampilkan.
-  // Logikanya: tampilkan jika role BUKAN "ANALIS".
-  // Sesuaikan 'ANALIS' dengan nama role yang kamu gunakan jika berbeda.
-  const showCustomerName = session?.user?.role !== "ANALIS";
+  console.log("SESSION DATA:", session);
+  const showCustomerName = session?.user?.role !== "analis";
   // ------------------------------------
 
   return (
